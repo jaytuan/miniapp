174c5210f9127b44add074d5facf487f17830725
liujiantuan <jrliujiantuan@jd.com>
2019-04-10 19:55:36 +0800

    add-单选图片按钮

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,52 +1,25 @@
 //index.js
 
-// 测试git命令行
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 用户信息
+    userInfo: null
   },
   onLoad: function () {
-    this.getUserInfoSwitch()
+
   },
-  getUserInfoSwitch () {
-    if (app.globalData.userInfo) {
+  // 单选方法
+  singleVote (e) {
+    console.log(e)
+    if (e && e.detail) {
+      app.globalData.userInfo = e.detail.userInfo
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+        userInfo: e.detail.userInfo
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
+
 })
